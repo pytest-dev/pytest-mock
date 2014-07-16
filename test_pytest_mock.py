edb46315d27c9f58e9b19bc2482a1db3b090ef77
Bruno Oliveira <nicoddemus@gmail.com>
2014-07-16 20:54:58 -0300

    adding support for patch.object

--- test_pytest_mock.py
+++ test_pytest_mock.py
@@ -7,21 +7,30 @@ class MockFixture(object):
 
     def __init__(self):
         self._patches = []
-        self.patch = self._PatchWrapper(self._patches)
+        self.patch = self._Patcher(self._patches)
 
     def stopall(self):
         for p in self._patches:
             p.stop()
         self._patches[:] = []
 
-    class _PatchWrapper(object):
-
+    class _Patcher(object):
         def __init__(self, patches):
             self._patches = patches
 
         def object(self, *args, **kwargs):
             import mock
+
             p = mock.patch.object(*args, **kwargs)
+            mocked = p.start()
+            self._patches.append(p)
+            return mocked
+
+
+        def __call__(self, *args, **kwargs):
+            import mock
+
+            p = mock.patch(*args, **kwargs)
             mocked = p.start()
             self._patches.append(p)
             return mocked
@@ -35,7 +44,6 @@ def mock():
 
 
 class UnixFS(object):
-
     @classmethod
     def rm(cls, filename):
         os.remove(filename)
@@ -45,12 +53,30 @@ class UnixFS(object):
         return os.listdir(path)
 
 
-def test_fixture(mock, tmpdir):
-    mock.patch.object(os, 'remove')
-    (tmpdir / 'foo.txt').ensure()
-    UnixFS.rm(tmpdir / 'foo.txt')
-    os.remove.assert_called_once_with(tmpdir / 'foo.txt')
-    assert os.path.isfile(str(tmpdir / 'foo.txt'))
-    mock.stopall()
-    UnixFS.rm(str(tmpdir / 'foo.txt'))
-    assert not os.path.isfile(str(tmpdir / 'foo.txt'))
+@pytest.fixture
+def check_unix_fs_mocked(tmpdir, mock):
+    def check(mocked_rm):
+        (tmpdir / 'foo.txt').ensure()
+        UnixFS.rm(tmpdir / 'foo.txt')
+        os.remove.assert_called_once_with(tmpdir / 'foo.txt')
+        mocked_rm.assert_called_once_with(tmpdir / 'foo.txt')
+        assert os.path.isfile(str(tmpdir / 'foo.txt'))
+        mock.stopall()
+        UnixFS.rm(str(tmpdir / 'foo.txt'))
+        assert not os.path.isfile(str(tmpdir / 'foo.txt'))
+
+    return check
+
+
+def mock_using_patch_object(mock):
+    return mock.patch.object(os, 'remove')
+
+def mock_using_patch(mock):
+    return mock.patch('os.remove')
+
+@pytest.mark.parametrize('mock_fs', [mock_using_patch_object, mock_using_patch],
+)
+def test_fixture(mock_fs, mock, check_unix_fs_mocked):
+    mocked_rm = mock_fs(mock)
+    #mocked_rm = mock.patch(os, 'remove')
+    check_unix_fs_mocked(mocked_rm)

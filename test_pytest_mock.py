1ad4d7e1707acfaafd79fb5d9538bd00b9b2df6a
Bruno Oliveira <nicoddemus@gmail.com>
2014-07-16 21:10:46 -0300

    adding support for patch.multiple

--- test_pytest_mock.py
+++ test_pytest_mock.py
@@ -4,7 +4,6 @@ import pytest
 
 
 class MockFixture(object):
-
     def __init__(self):
         self._patches = []
         self.patch = self._Patcher(self._patches)
@@ -22,6 +21,15 @@ class MockFixture(object):
             import mock
 
             p = mock.patch.object(*args, **kwargs)
+            mocked = p.start()
+            self._patches.append(p)
+            return mocked
+
+
+        def multiple(self, *args, **kwargs):
+            import mock
+
+            p = mock.patch.multiple(*args, **kwargs)
             mocked = p.start()
             self._patches.append(p)
             return mocked
@@ -56,13 +64,23 @@ class UnixFS(object):
 @pytest.fixture
 def check_unix_fs_mocked(tmpdir, mock):
     def check(mocked_rm, mocked_ls):
+        assert mocked_rm is os.remove
+        assert mocked_ls is os.listdir
+
         file_name = tmpdir / 'foo.txt'
         file_name.ensure()
+
         UnixFS.rm(str(file_name))
-        os.remove.assert_called_once_with(str(file_name))
         mocked_rm.assert_called_once_with(str(file_name))
         assert os.path.isfile(str(file_name))
+
+        mocked_ls.return_value = ['bar.txt']
+        assert UnixFS.ls(str(tmpdir)) == ['bar.txt']
+        mocked_ls.assert_called_once_with(str(tmpdir))
+
         mock.stopall()
+
+        assert UnixFS.ls(str(tmpdir)) == ['foo.txt']
         UnixFS.rm(str(file_name))
         assert not os.path.isfile(str(file_name))
 
@@ -77,7 +95,15 @@ def mock_using_patch(mock):
     return mock.patch('os.remove'), mock.patch('os.listdir')
 
 
-@pytest.mark.parametrize('mock_fs', [mock_using_patch_object, mock_using_patch],
+def mock_using_patch_multiple(mock):
+    from mock import DEFAULT
+
+    r = mock.patch.multiple('os', remove=DEFAULT, listdir=DEFAULT)
+    return r['remove'], r['listdir']
+
+
+@pytest.mark.parametrize('mock_fs', [mock_using_patch_object, mock_using_patch,
+                                     mock_using_patch_multiple],
 )
 def test_fixture(mock_fs, mock, check_unix_fs_mocked):
     mocked_rm, mocked_ls = mock_fs(mock)

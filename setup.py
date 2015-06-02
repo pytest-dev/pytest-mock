7f001038d24c68b7018155fefe0ad3bc00abe33e
Bruno Oliveira <nicoddemus@gmail.com>
2015-06-02 18:01:11 -0300

    Bump to version 0.6.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 3):
 
 setup(
     name='pytest-mock',
-    version='0.5.0',
+    version='0.6.0',
     entry_points={
         'pytest11': ['pytest_mock = pytest_mock'],
     },

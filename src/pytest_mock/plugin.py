5b1376ec59d2faa255e6fadfd234ae90feec17f8
Bruno Oliveira <nicoddemus@gmail.com>
2020-08-06 19:24:01 -0300

    Add mypy to linting and pre-commit

--- src/pytest_mock/plugin.py
+++ src/pytest_mock/plugin.py
@@ -206,7 +206,7 @@ session_mocker = pytest.yield_fixture(scope="session")(_mocker)
 
 
 _mock_module_patches = []
-_mock_module_originals = {}
+_mock_module_originals = {}  # type: Dict[str, Any]
 
 
 def assert_wrapper(__wrapped_mock_method__, *args, **kwargs):

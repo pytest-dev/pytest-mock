1662868e4f2be94c9bddb79a8f9b1fb8b64f6ea5
Bruno Oliveira <bruno@esss.com>
2024-01-31 10:23:43 -0300

    Fix ruff isort configuration
    In addition, remove other hooks which are not needed now as they are handled by ruff.

--- src/pytest_mock/__init__.py
+++ src/pytest_mock/__init__.py
@@ -1,11 +1,11 @@
+from pytest_mock.plugin import MockerFixture
+from pytest_mock.plugin import PytestMockWarning
 from pytest_mock.plugin import class_mocker
 from pytest_mock.plugin import mocker
-from pytest_mock.plugin import MockerFixture
 from pytest_mock.plugin import module_mocker
 from pytest_mock.plugin import package_mocker
 from pytest_mock.plugin import pytest_addoption
 from pytest_mock.plugin import pytest_configure
-from pytest_mock.plugin import PytestMockWarning
 from pytest_mock.plugin import session_mocker
 
 MockFixture = MockerFixture  # backward-compatibility only (#204)

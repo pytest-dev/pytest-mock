b30cd6629f479d4e83ca4cac3daa81f53646301a
Gabriel <gabriel59kg@gmail.com>
2024-03-13 11:27:33 -0400

    export `MockType`

--- src/pytest_mock/__init__.py
+++ src/pytest_mock/__init__.py
@@ -7,12 +7,14 @@ from pytest_mock.plugin import package_mocker
 from pytest_mock.plugin import pytest_addoption
 from pytest_mock.plugin import pytest_configure
 from pytest_mock.plugin import session_mocker
+from pytest_mock.plugin import MockType
 
 MockFixture = MockerFixture  # backward-compatibility only (#204)
 
 __all__ = [
     "MockerFixture",
     "MockFixture",
+    "MockType",
     "PytestMockWarning",
     "pytest_addoption",
     "pytest_configure",

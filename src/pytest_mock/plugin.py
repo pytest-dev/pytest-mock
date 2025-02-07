a76eb4d3a82e95b803baf3870c4928056bb5375d
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2025-02-07 13:31:11 +0100

    Update url in warning (#490)
    This was missed in #383

--- src/pytest_mock/plugin.py
+++ src/pytest_mock/plugin.py
@@ -264,7 +264,7 @@ class MockerFixture:
                         "Mocks returned by pytest-mock do not need to be used as context managers. "
                         "The mocker fixture automatically undoes mocking at the end of a test. "
                         "This warning can be ignored if it was triggered by mocking a context manager. "
-                        "https://pytest-mock.readthedocs.io/en/latest/remarks.html#usage-as-context-manager",
+                        "https://pytest-mock.readthedocs.io/en/latest/usage.html#usage-as-context-manager",
                         PytestMockWarning,
                         stacklevel=5,
                     )
